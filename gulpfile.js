d1c08b6481b81b500eaa1a3e4282ee1251c13de0
SOFIASEMCHUK <semchuk.s02@gmail.com>
2022-08-12 18:23:50 +0300

    page tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp')
 var browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
-
+const cleanCSS = require('gulp-clean-css');
 gulp.task('sass', function(done) {
     gulp.src("sass/*.sass")
         .pipe(sass().on('error', function (e) {
@@ -10,11 +10,13 @@ gulp.task('sass', function(done) {
         .pipe(sass())
         .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
-
-
     done();
 });
-
+gulp.task('minify-css', () => {
+    return gulp.src('css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('dist'));
+});
 gulp.task('serve', function(done) {
 
     browserSync.init({
@@ -22,6 +24,7 @@ gulp.task('serve', function(done) {
     });
 
     gulp.watch("sass/*.sass", gulp.series('sass'));
+    gulp.watch("sass/*.sass", gulp.series('minify-css'));
     gulp.watch("*.html").on('change', () => {
         browserSync.reload();
         done();
@@ -30,4 +33,4 @@ gulp.task('serve', function(done) {
 
     done();
 });
-gulp.task('default', gulp.series('sass', 'serve'));
+gulp.task('default', gulp.series('sass', 'minify-css','serve'));
